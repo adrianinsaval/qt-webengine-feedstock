368f56cd6e9fc213b1d76737b5f5b557fdbc787f
Adrian Insaurralde Avalos <adrianinsaval@gmail.com>
2024-06-25 15:50:47 -0400

    osx_arm64 python 2 hack

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,8 +9,16 @@ USED_BUILD_PREFIX=${BUILD_PREFIX:-${PREFIX}}
 echo USED_BUILD_PREFIX=${BUILD_PREFIX}
 
 # qtwebengine needs python 2
-mamba create --yes --prefix "${SRC_DIR}/python2_hack" --channel conda-forge --no-deps python=2
-export PATH=${SRC_DIR}/python2_hack/bin:${PATH}
+if [[ $(uname) == "Darwin" ]]; then
+    if [[ $(arch) == "arm64" ]]; then
+      brew install pyenv
+      pyenv install 2.7.18
+      export PATH="$(pyenv root)/shims:${PATH}"
+    fi
+else
+    mamba create --yes --prefix "${SRC_DIR}/python2_hack" --channel conda-forge --no-deps python=2
+    export PATH=${SRC_DIR}/python2_hack/bin:${PATH}
+fi
 
 if [[ $(uname) == "Linux" ]]; then
     ln -s ${GXX} g++ || true
